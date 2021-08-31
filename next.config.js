993c0e4deaf8ea54bff0ab8663ae9125812bf5a1
Hanison <yohanbae1040@gmail.com>
2021-08-31 09:31:07 -0400

    third

--- next.config.js
+++ next.config.js
@@ -3,4 +3,9 @@ module.exports = {
   images: {
     domains: ['image.tmdb.org'],
   },  
+  "extends": "next",
+  "rules": {
+    "react/no-unescaped-entities": "off",
+    "@next/next/no-page-custom-font": "off"
+  }
 }
